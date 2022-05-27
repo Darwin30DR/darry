d7530bd7dce41588efda2b8a943bd68642579ab6
Darwin30DR <104537710+Darwin30DR@users.noreply.github.com>
2022-05-27 01:21:10 -0500

    dd

--- js/main.js
+++ js/main.js
@@ -16,8 +16,8 @@ if(  document.querySelector(".style-switcher").classList.contains("open"))
 /*========================================= cambiar icono y fuente  =================*/
 const dayNight = document.querySelector(".day-night");
 dayNight.addEventListener("click", () =>{
-    dayNight.querySelector("i").classList.toggle("icon-quill");
-    dayNight.querySelector("i").classList.toggle("icon-home3");
+    dayNight.querySelector("i").classList.toggle("icon-contrast");
+    dayNight.querySelector("i").classList.toggle("icon-sun");
     document.body.classList.toggle("dark");
 })
 window.addEventListener("load", () => {
@@ -54,30 +54,30 @@ window.addEventListener("load", () => {
 const color1 = document.querySelector(".color1");
     color1.addEventListener("click", () =>{
     color1.querySelector("span").classList.toggle("icon-home3");
-    color1.querySelector("span").classList.toggle("icon-facebook");
+    color1.querySelector("span").classList.toggle("icon-happy2");
     document.body.classList.toggle("darwin");
 })
 const color2 = document.querySelector(".color2");
     color2.addEventListener("click", () =>{
     color2.querySelector("span").classList.toggle("icon-home3");
-    color2.querySelector("span").classList.toggle("icon-instagram");
+    color2.querySelector("span").classList.toggle("icon-smile");
     document.body.classList.toggle("darwin2");
 })
 const color3 = document.querySelector(".color3");
     color3.addEventListener("click", () =>{
     color3.querySelector("span").classList.toggle("icon-home3");
-    color3.querySelector("span").classList.toggle("icon-spotify");
+    color3.querySelector("span").classList.toggle("icon-smile2");
     document.body.classList.toggle("darwin3");
 })
 const color4 = document.querySelector(".color4");
     color4.addEventListener("click", () =>{
     color4.querySelector("span").classList.toggle("icon-home3");
-    color4.querySelector("span").classList.toggle("icon-twitter");
+    color4.querySelector("span").classList.toggle("icon-cool");
     document.body.classList.toggle("darwin4");
 })
 const color5 = document.querySelector(".color5");
     color5.addEventListener("click", () =>{
     color5.querySelector("span").classList.toggle("icon-home3");
-    color5.querySelector("span").classList.toggle("icon-github");
+    color5.querySelector("span").classList.toggle("icon-flickr4");
     document.body.classList.toggle("darwin5");
 })
